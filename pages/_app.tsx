5e163cb09b17cc3f0fca03ba1064c3ac96278726
Kong <duguyihou@gmail.com>
2022-07-20 21:32:21 +1000

    feat: setup graphql (#14)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 
 import { Layout } from '../components/layout'
+import { client } from '../graphql'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   )
 }
 
