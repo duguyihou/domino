017c48d34697c2ff65176caeacceba03a252df2e
kong <duguyihou@gmail.com>
2022-07-24 18:45:21 +1000

    refactor(column): remove unsed code

--- components/column/Column.tsx
+++ components/column/Column.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, LegacyRef } from 'react'
+import React, { forwardRef } from 'react'
 
 import classNames from 'classnames'
 
@@ -6,74 +6,27 @@ import { Handle, Remove } from '../common'
 import styles from './Column.module.scss'
 import { ColumnProps } from './Column.types'
 
-const Column = forwardRef<HTMLDivElement | HTMLButtonElement, ColumnProps>(
+const Column = forwardRef<HTMLDivElement, ColumnProps>(
   (columnProps: ColumnProps, ref) => {
-    const {
-      children,
-      handleProps,
-      horizontal,
-      hover,
-      onClick,
-      onRemove,
-      label,
-      placeholder,
-      style,
-      scrollable,
-      shadow,
-      unstyled,
-      ...props
-    } = columnProps
+    const { children, handleProps, hover, onRemove, label, style } = columnProps
     const componentClassName = classNames(
       styles.container,
-      unstyled && styles.unstyled,
-      horizontal && styles.horizontal,
-      hover && styles.hover,
-      placeholder && styles.placeholder,
-      scrollable && styles.scrollable,
-      shadow && styles.shadow
+      hover && styles.hover
     )
-    if (onClick) {
-      return (
-        <button
-          {...props}
-          ref={ref as LegacyRef<HTMLButtonElement> | undefined}
-          style={style}
-          className={componentClassName}
-          onClick={onClick}
-          tabIndex={0}
-        >
-          {label && (
-            <div className={styles.header}>
-              {label}
-              <div className={styles.actions}>
-                {onRemove ? <Remove onClick={onRemove} /> : undefined}
-                <Handle {...handleProps} />
-              </div>
-            </div>
-          )}
-          {placeholder ? children : <ul>{children}</ul>}
-        </button>
-      )
-    }
     return (
-      <div
-        {...props}
-        ref={ref as LegacyRef<HTMLDivElement> | undefined}
-        style={style}
-        className={componentClassName}
-        onClick={onClick}
-        tabIndex={undefined}
-      >
+      <div ref={ref} style={style} className={componentClassName}>
         {label && (
           <div className={styles.header}>
             {label}
             <div className={styles.actions}>
-              {onRemove ? <Remove onClick={onRemove} /> : undefined}
+              {onRemove && (
+                <Remove className={styles.remove} onClick={onRemove} />
+              )}
               <Handle {...handleProps} />
             </div>
           </div>
         )}
-        {placeholder ? children : <ul>{children}</ul>}
+        <ul>{children}</ul>
       </div>
     )
   }
--- components/column/Column.types.ts
+++ components/column/Column.types.ts
@@ -4,13 +4,7 @@ export type ColumnProps = {
   children: ReactNode
   label?: string
   style?: CSSProperties
-  horizontal?: boolean
   hover?: boolean
-  handleProps?: HTMLAttributes<HTMLDivElement | HTMLButtonElement>
-  scrollable?: boolean
-  shadow?: boolean
-  placeholder?: boolean
-  unstyled?: boolean
-  onClick?(): void
+  handleProps?: HTMLAttributes<HTMLButtonElement>
   onRemove?(): void
 }
