de76021e187bf1e1cf0a09f916a53b8d6a512c63
kong <duguyihou@gmail.com>
2022-07-27 09:39:22 +1000

    refactor: wip

---
+++ mock/index.ts
@@ -0,0 +1,92 @@
+export const initialColumns = {
+  Todo: [
+    {
+      id: 'a1',
+      title: 'A1',
+      description: 'desc for A1',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'a2',
+      title: 'A2',
+      description: 'desc for A2',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'a3',
+      title: 'A3',
+      description: 'desc for A3',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'a4',
+      title: 'A4',
+      description: 'desc for A4',
+      label: 'epic',
+      priority: 'low',
+    },
+  ],
+  Doing: [
+    {
+      id: 'b1',
+      title: 'B1',
+      description: 'desc for B1',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'b2',
+      title: 'B2',
+      description: 'desc for B2',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'b3',
+      title: 'B3',
+      description: 'desc for B3',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'b4',
+      title: 'B4',
+      description: 'desc for B4',
+      label: 'epic',
+      priority: 'low',
+    },
+  ],
+  Done: [
+    {
+      id: 'c1',
+      title: 'C1',
+      description: 'desc for C1',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'c2',
+      title: 'C2',
+      description: 'desc for C2',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'c3',
+      title: 'C3',
+      description: 'desc for C3',
+      label: 'epic',
+      priority: 'low',
+    },
+    {
+      id: 'c4',
+      title: 'C4',
+      description: 'desc for C4',
+      label: 'epic',
+      priority: 'low',
+    },
+  ],
+}
